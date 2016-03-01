b3098c85f298a3de37676b05f76dc5a1c45b76e5
Wolfgang Weissinger <wolfgang@weissinger.wien>
2016-03-01 17:50:20 +0100

    Task3 hinzugefuegt

---
+++ tasks/task3/task3/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    /// <summary>
+    /// Erstellt neue Fussballerprofile
+    /// </summary>
+    class footballplayer
+    {
+        public string name;
+        public string nationality;
+        private int goals;
+        private int yellowcards;
+        private bool IsBanned = false;
+
+        public string defName
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                name = value;
+            }
+        }
+
+        public footballplayer(string newname, string newnationality, int newgoals = 0)
+        {
+            name = newname;
+            nationality = newnationality;
+            if (newgoals < 0) throw new Exception("Fehler. Negative Toranzahl");
+            goals = newgoals;
+        }
+
+        public void UpdateGoals(int newgoals)
+        {
+            if (newgoals > goals) goals = newgoals;
+            else throw new Exception();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2}", this.name, this.nationality, this.goals);
+        }
+    }
+
+    public interface Profile
+    {
+        /// <summary>
+        /// Gets a description of the basic player information.
+        /// </summary>
+        string name { get; }
+
+        /// <summary>
+        /// Gets the item's price in the specified currency.
+        /// </summary>
+        decimal GetGoals(int goals);
+    }
+
+
+
+    class goalkeeper : footballplayer
+    {
+        private int height;
+
+        /// <summary>
+        /// Creates profile of a Goalkeeper.
+        /// </summary>
+        public goalkeeper(string newname, string newnationality, int newgoals = 0, int newheight)
+        {
+            name = newname;
+            nationality = newnationality;
+            if (newgoals < 0) throw new Exception("Fehler. Negative Toranzahl");
+            goals = newgoals;
+            if (height < 100) throw new Exception("Fehler. Tormann ist ein Zwerg");
+            height = newheight;
+
+        }
+
+        /// <summary>
+        /// Height of Goalkeeper.
+        /// </summary>
+        public decimal height { get; }
+
+
+
+        /// <summary>
+        /// If the Player receives the 5th yellow card, he gets suspended for one match
+        /// </summary>
+        public void Banned()
+        {
+            if (IsBanned == true) throw new Exception("Spieler ist bereits gesperrt");
+            yellowcards++;
+            if (yellowcards == 5) IsBanned = true;
+        }
+
+    }
+
+
+}
+
+
+
---
+++ tasks/task3/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2;
+
+
+namespace Task2
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            var ronaldo = new footballplayer("ronaldo", "POR");
+            var messi = new footballplayer("messi", "ARG");
+            Console.WriteLine("Nationalitaet Ronaldo: {0}", ronaldo.nationality);
+            Console.WriteLine("{0}", messi);
+
+            int tore = 200;
+            messi.UpdateGoals(tore);
+            Console.WriteLine("{0}", messi);
+        }
+    }
+}
