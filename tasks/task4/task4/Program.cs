a17762609985f4e63a42e37db49a4efe5f3a0348
Wolfgang Weissinger <wolfgang@weissinger.wien>
2016-03-08 14:11:18 +0100

    Task4 hinzugefuegt

---
+++ tasks/task4/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RentalObjects_bouncer[] bouncerarray = new RentalObjects_bouncer[5];
+            bouncerarray[0] = new RentalObjects_bouncer ( "Babytreff", 73, 79, 119, 20 );
+            bouncerarray[1] = new RentalObjects_bouncer("Happy Train", 73, 79, 119, 15);
+            bouncerarray[2] = new RentalObjects_bouncer("SkyKids", 73, 79, 119, 30);
+            bouncerarray[3] = new RentalObjects_bouncer("Fantasia", 91, 99, 146, 80);
+            bouncerarray[4] = new RentalObjects_bouncer("Schwammerl", 91, 99, 146, 80);
+
+            var c = new RentalObjects_bouncer("SkyKids", 73, 79, 119, 20);
+
+            var a = JsonConvert.SerializeObject(c);
+            Console.WriteLine(a);
+
+            
+            RentalObjects_bouncer b = JsonConvert.DeserializeObject<RentalObjects_bouncer>(a);
+            Console.WriteLine(b.prop_name);
+            Console.WriteLine(b.prop_price_weekdays);
+
+
+        }
+    }
+}
---
+++ tasks/task4/task4/Tests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace task3._1
+{
+    [TestFixture]
+    public class Tests
+    {
+        // Bouncers
+        [Test]
+        public void Weekday_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_bouncer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_weekdays == 36);
+        }
+        [Test]
+        public void Weekend_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_bouncer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_weekends == 39);
+        }
+        [Test]
+        public void F48h_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_bouncer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_48h == 59);
+        }
+        [Test]
+        public void No_negative_price_weekdays()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_bouncer("Babytreff", -36, 39, 59, 10); });
+        }
+
+        [Test]
+        public void No_negative_price_weekends()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_bouncer("Babytreff", 36, -39, 59, 10); });
+        }
+
+        [Test]
+        public void No_negative_price_48h()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_bouncer("Babytreff", 36, 39, -59, 10); });
+        }
+        [Test]
+        public void No_negative_weight()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_bouncer("Babytreff", 36, 39, 59, -10); });
+        }
+        [Test]
+        public void Atleast_one_day_booking_delivery()
+        {
+            var y = new RentalObjects_bouncer("Babytreff", 36, 39, 59, 10);
+
+            Assert.Catch(() => {
+                
+                y.price_calculation_delivery(y.prop_price_weekdays, -3); });
+        }
+        [Test]
+        public void Atleast_one_day_booking_pickup()
+        {
+            var y = new RentalObjects_bouncer("Babytreff", 36, 39, 59, 10);
+
+            Assert.Catch(() => {
+
+                y.price_calculation_pickup(y.prop_price_weekdays, -3);
+            });
+        }
+
+        // Skydancers
+        [Test]
+        public void Skydancer_Weekday_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_skydancer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_weekdays == 36);
+        }
+        [Test]
+        public void Skydancer_Weekend_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_skydancer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_weekends == 39);
+        }
+        [Test]
+        public void Skydancer_48h_Price_Compare_with_Initialization()
+        {
+            var x = new RentalObjects_skydancer("Babytreff", 36, 39, 59, 10);
+            Assert.IsTrue(x.prop_price_48h == 59);
+        }
+        [Test]
+        public void Skydancer_No_negative_price_weekdays()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_skydancer("Babytreff", -36, 39, 59, 10); });
+        }
+
+        [Test]
+        public void Skydancer_No_negative_price_weekends()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_skydancer("Babytreff", 36, -39, 59, 10); });
+        }
+
+        [Test]
+        public void Skydancer_No_negative_price_48h()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_skydancer("Babytreff", 36, 39, -59, 10); });
+        }
+        [Test]
+        public void Skydancer_No_negative_weight()
+        {
+            Assert.Catch(() => { var x = new RentalObjects_skydancer("Babytreff", 36, 39, 59, -10); });
+        }
+    }
+}
