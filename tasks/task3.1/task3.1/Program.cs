07f89e67413f9af059f574098134f4c99e46b526
Wolfgang Weissinger <wolfgang@weissinger.wien>
2016-03-08 10:47:04 +0100

    Task 3.1 hinzugefuegt

---
+++ tasks/task3.1/task3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RentalObjects_bouncer[] bouncerarray = new RentalObjects_bouncer[5];
+            bouncerarray[0] = new RentalObjects_bouncer ( "Babytreff", 73, 79, 119, 20 );
+            bouncerarray[1] = new RentalObjects_bouncer("Happy Train", 73, 79, 119, 15);
+            bouncerarray[2] = new RentalObjects_bouncer("SkyKids", 73, 79, 119, 30);
+            bouncerarray[3] = new RentalObjects_bouncer("Fantasia", 91, 99, 146, 80);
+            bouncerarray[4] = new RentalObjects_bouncer("Schwammerl", 91, 99, 146, 80);
+
+            RentalObjects_bouncer babytreff = new RentalObjects_bouncer("Babytreff Luftburg", 73, 79, 119, 20);
+            RentalObjects_bouncer happytrain = new RentalObjects_bouncer("Happy Train", 73, 79, 119, 15);
+
+            Console.WriteLine("Name von Babytreff: {0}", bouncerarray[0].prop_name);
+            Console.WriteLine("Wochentagspreis von Babytreff: Euro {0},-", bouncerarray[1].prop_price_weekdays);
+            Console.WriteLine("Name von HappyTrain: {0}", bouncerarray[2].prop_name);
+            Console.WriteLine("Wochenendpreis von HappyTrain: {0} Euro ", bouncerarray[3].prop_price_weekends);  
+
+            var preis = bouncerarray[0].price_calculation_pickup(bouncerarray[0].prop_price_weekdays, 5);
+            var preis3 = bouncerarray[3].price_calculation_pickup(bouncerarray[3].prop_price_weekdays, 5);
+
+            Console.WriteLine("Ihr Preis für 5 Wochentage Babytreff: {0}", preis);
+            Console.WriteLine("Ihr Preis für 5 Wochentage {1}: {0}", preis3, bouncerarray[3].prop_name);
+
+
+        }
+    }
+}
---
+++ tasks/task3.2/task4/RentalObjects.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3.2 {
+
+    interface calculation
+{
+    double price_calculation_pickup(double price, int days);
+    double price_calculation_delivery(double price, int days);
+}
+
+
+class calculation_regular_customer : calculation
+{
+    public override double price_calculation_pickup(double price, int days)
+    {
+        return (price * days) * 0.9;
+    }
+    public override double price_calculation_delievery(double price, int days)
+    {
+        return (price * days) * 0.9 + 25;
+    }
+}
+
+class calculation_new_customer : calculation
+{
+    public override double price_calculation_pickup(double price, int days)
+    {
+        return (price * days);
+    }
+    public override double price_calculation_delievery(double price, int days)
+    {
+        return (price * days) + 30;
+    }
+}
+
+
+class RentalObjects
+    {
+        private string name;
+        private double price_weekdays;
+        private double price_weekends;
+        private double price_48h;
+        private int weight;
+
+        // Properties
+        public string prop_name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception("Namensfeld ist leer");
+                else name = value;
+            }
+        }
+        public double prop_price_weekdays
+        {
+            get { return price_weekdays; }
+            set
+            {
+                if (value < 0) throw new Exception("Preis darf nicht negativ sein!");
+                else price_weekdays = value;
+            }
+        }
+        public double prop_price_weekends
+        {
+            get { return price_weekends; }
+            set
+            {
+                if (value < price_weekdays) throw new Exception ("Preis am Wochenende muss höher sein als unter der Woche");
+                
+                else price_weekends = value;
+            }
+        }
+        public double prop_price_48h
+        {
+            get { return price_48h; }
+            set
+            {
+                if (value < price_weekends) throw new Exception("Preis für 48h müss höher sein als Wochenendpreis (und Wochentagspreis)");
+
+                else price_48h = value;
+            }
+        }
+        public int prop_weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value < 0) throw new Exception("Gewicht darf nicht negativ sein");
+                else weight = value;
+            }
+        }
+
+        // Methods
+        public double price_calculation (double price, int days)
+        {
+            if (days < 1) throw new Exception("Buchungsdauer muss mindestens einen Tag betragen");
+            if (days > 2)
+            {
+                return (price * days) * 0.9;
+            }
+            return price * days;
+        }
+
+        // Constructor
+        public RentalObjects (string newname, double newprice_weekdays, double newprice_weekends, double newprice_48h, int newweight)
+        {
+            if (string.IsNullOrEmpty(newname)) throw new Exception("Namensfeld ist leer");
+            else name = newname;
+            if (newprice_weekdays < 0) throw new Exception("Preis darf nicht negativ sein");
+            else price_weekdays = newprice_weekdays;
+            if (newprice_weekends < price_weekdays) throw new Exception("Preis am Wochenende muss höher sein als an einem Wochentag");
+            else price_weekends = newprice_weekends;
+            if (newprice_48h < price_weekends) throw new Exception("Preis für 48h muss größer sein als Wochenendpreis");
+            else price_48h = newprice_48h;
+            if (newweight < 0) throw new Exception("Gewicht darf nicht negativ sein. Fragen Sie Isaac Newton um Hilfe.");
+        }
+    }
+}
